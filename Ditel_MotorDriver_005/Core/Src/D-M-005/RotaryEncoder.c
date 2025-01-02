82040a1a684daa1fc5a53c7d1e5aeecd65bb4cbf
Ditel252 <Ditel252@gmail.com>
2025-01-02 20:18:14 +0900

    v2.0.26

--- Ditel_MotorDriver_005/Core/Src/D-M-005/RotaryEncoder.c
+++ Ditel_MotorDriver_005/Core/Src/D-M-005/RotaryEncoder.c
@@ -21,20 +21,21 @@ void _RotaryEncoderInit(_ROTARY_ENCODER_SETTING *__RotaryEncoderSetting){
 	_RotaryEncoderSetting.__MesureTime_TIM = __RotaryEncoderSetting->__MesureTime_TIM;
 }
 
+//Hard coding for faster processing
 _ROTARY_ENCODER_RESULT _RotaryEncoder_Get1Cycle_TimePeriod(){
 	_ROTARY_ENCODER_RESULT _result;
 
 	__HAL_TIM_SET_COUNTER(_RotaryEncoderSetting.__MesureTime_TIM, 0);
 
-	while(HAL_GPIO_ReadPin(_RotaryEncoderSetting.__RotaryEncoderZ_GpioPort, _RotaryEncoderSetting.__RotaryEncoderZ_Pin) == GPIO_PIN_SET){
-		if(__HAL_TIM_GET_COUNTER(_RotaryEncoderSetting.__MesureTime_TIM) > __ROTARY_ENCODER_HARF_CYCLE_MAX_TIMER_COUNT){
+	while((GPIOA -> IDR & 0b100000)){
+		if(TIM1->CNT > __ROTARY_ENCODER_HARF_CYCLE_MAX_TIMER_COUNT){
 			_result._isSuccessGet1CycleTimePerioCount = false;
 			return _result;
 		}
 	}
 
-	while(HAL_GPIO_ReadPin(_RotaryEncoderSetting.__RotaryEncoderZ_GpioPort, _RotaryEncoderSetting.__RotaryEncoderZ_Pin) == GPIO_PIN_RESET){
-		if(__HAL_TIM_GET_COUNTER(_RotaryEncoderSetting.__MesureTime_TIM) > __ROTARY_ENCODER_HARF_CYCLE_MAX_TIMER_COUNT){
+	while(!(GPIOA -> IDR & 0b100000)){
+		if(TIM1->CNT > __ROTARY_ENCODER_HARF_CYCLE_MAX_TIMER_COUNT){
 			_result._isSuccessGet1CycleTimePerioCount = false;
 			return _result;
 		}
@@ -42,21 +43,21 @@ _ROTARY_ENCODER_RESULT _RotaryEncoder_Get1Cycle_TimePeriod(){
 
 	__HAL_TIM_SET_COUNTER(_RotaryEncoderSetting.__MesureTime_TIM, 0);
 
-	while(HAL_GPIO_ReadPin(_RotaryEncoderSetting.__RotaryEncoderZ_GpioPort, _RotaryEncoderSetting.__RotaryEncoderZ_Pin) == GPIO_PIN_SET){
-		if(__HAL_TIM_GET_COUNTER(_RotaryEncoderSetting.__MesureTime_TIM) > __ROTARY_ENCODER_HARF_CYCLE_MAX_TIMER_COUNT){
+	while((GPIOA -> IDR & 0b100000)){
+		if(TIM1->CNT > __ROTARY_ENCODER_HARF_CYCLE_MAX_TIMER_COUNT){
 			_result._isSuccessGet1CycleTimePerioCount = false;
 			return _result;
 		}
 	}
 
-	while(HAL_GPIO_ReadPin(_RotaryEncoderSetting.__RotaryEncoderZ_GpioPort, _RotaryEncoderSetting.__RotaryEncoderZ_Pin) == GPIO_PIN_RESET){
-		if(__HAL_TIM_GET_COUNTER(_RotaryEncoderSetting.__MesureTime_TIM) > __ROTARY_ENCODER_HARF_CYCLE_MAX_TIMER_COUNT){
+	while(!(GPIOA -> IDR & 0b100000)){
+		if(TIM1->CNT > __ROTARY_ENCODER_HARF_CYCLE_MAX_TIMER_COUNT){
 			_result._isSuccessGet1CycleTimePerioCount = false;
 			return _result;
 		}
 	}
 
-	_result._RotaryEncoder_1CycleTimePeriodCount = __HAL_TIM_GET_COUNTER(_RotaryEncoderSetting.__MesureTime_TIM);
+	_result._RotaryEncoder_1CycleTimePeriodCount = TIM1->CNT;
 	_result._isSuccessGet1CycleTimePerioCount = true;
 
 	return _result;
